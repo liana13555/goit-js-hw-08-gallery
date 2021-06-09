24dbdc4a1076f19f155400945e40e8432e6b48b6
Liana Baryshova <barishovaliana@gmail.com>
2021-06-09 19:01:52 +0200

    Add delegation and modal

--- gallery.js
+++ gallery.js
@@ -14,6 +14,7 @@ const galleryContainer = document.querySelector('.js-gallery');
 const cardsMarkup = makePictureCardMarkup(pictures);
 
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+galleryContainer.addEventListener('click', onGalleryContainerClick);
 
 function makePictureCardMarkup(pictures) {
     return pictures.map(({ preview, original, description }) => {
@@ -33,5 +34,78 @@ function makePictureCardMarkup(pictures) {
 </li>
     `;
     }).join('');
-    
+
 }
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+
+    const isGalleryImage = evt.target.classList.contains('gallery__image');
+
+    if (!isGalleryImage) {
+        return;
+    }
+
+    // функция открытия модалки
+    onOpenLightbox();
+
+    refs.lightboxImage.src = evt.target.dataset.source;
+    refs.lightboxImage.alt = evt.target.alt;
+
+
+   console.log(evt.target);
+}
+
+/* 
+Модальное окно для полноразмерного изображения
+      Для того чтобы открыть, необходимо добавить на div.lightbox CSS-класс is-open
+*/
+
+const refs = {
+    lightbox: document.querySelector('.js-lightbox'),
+    closeLightboxBtn: document.querySelector('[data-action="close-lightbox"]'),
+    lightboxImage: document.querySelector('.lightbox__image'),
+}
+
+refs.lightboxImage.addEventListener('click', onOpenLightbox);
+
+function onOpenLightbox() {
+    window.addEventListener('keydown', onEscKeyPress);    
+    refs.lightbox.classList.add('is-open');    
+}
+
+refs.closeLightboxBtn.addEventListener('click', onCloseLightbox);
+
+function onCloseLightbox() {
+    window.removeEventListener('keydown', onEscKeyPress);
+    refs.lightbox.classList.remove('is-open');
+    refs.lightboxImage.src = "";
+    refs.lightboxImage.alt = "";
+}
+
+//Закрытие модального окна по клику на div.lightbox__overlay.
+
+refs.lightbox.addEventListener('click', onLightboxClick);
+
+function onLightboxClick(evt) {
+  if (evt.target !== refs.lightboxImage) {
+        onCloseLightbox();
+    }    
+}
+
+// Закрытие модального окна по нажатию клавиши ESC
+
+function onEscKeyPress(evt) {
+    if (evt.code === 'Escape') {
+     onCloseLightbox();   
+    }    
+}
+
+// Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо"
+window.addEventListener('keydown', onPrevNextPress);
+
+function onPrevNextPress(evt) {
+     if (evt.code === 'ArrowRight') {     
+    }
+}
+
